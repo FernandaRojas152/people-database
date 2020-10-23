6c192b2bece05c3d805aa4234bc4a5f31f7feaa7
FernandaRojas152 <FernandaRojas152@hotmail.com>
2020-10-23 00:21:03 -0500

    balancing factor fixed

--- src/binarySearchTree/BinarySearchTree.java
+++ src/binarySearchTree/BinarySearchTree.java
@@ -9,158 +9,80 @@ import java.util.Collection;
  * @param <V> Value
  */
 
-public class BinarySearchTree<K extends Comparable<K>, V> implements BinarySearchTreeOperations<K, V>{
+public class BinarySearchTree<K extends Comparable<K>, V>{
 	
 	private Node<K, V> root;
 	private int weight;
 	private int height;
 	
+	public Node<K, V> getRoot() {
+		return root;
+	}
+
+	public void setRoot(Node<K, V> root) {
+		this.root = root;
+	}
+
+	public void setWeight(int weight) {
+		this.weight = weight;
+	}
+
+	public void setHeight(int height) {
+		this.height = height;
+	}
+
 	/**
 	 * BinarySearchTree constructor
 	 */
 	
 	public BinarySearchTree() {
+		root= null;
 	}
-<<<<<<< HEAD
+	
+	public boolean add(K k, V v) {
+		Node<K, V> tA = new Node<>(k,v);
+		return addNode(tA);
+	}
+	
 	
 	/**
-	 * Adds a new node to the binary search tree
-	 * <b>post:</b> A new node has been added<br>
-	 * @param k Key
-	 * @param v Value
-	 * @throws Exception
+	 * Adds a node in the tree, provided that it's key hasn't been added yet.
+	 * @param ta The given node to be added in the tree.
 	 */
-	
-	public void addNode(K k, V v) throws Exception {
+	protected boolean addNode(Node<K, V> tA) {
+		if(searchNode(tA.getK()) != null) {
+			return false;
+		}		
 		
-		Node<K, V> node = new Node<>(k, v);
-		
-		if(root==null)
-			root = node;
-		else
-			root.addNode(node);
-	}
-	
-	/**
-	 * Updates the Value v of the node with Key k
-	 * @param k Key
-	 * @param v Value
-	 */
-	
-	public void updateNode(K k, V v) {
-		searchNode(k).setV(v);
-	}
-	
-	/**
-	 * Searches a node in the binary search tree
-	 * @param k Key
-	 * @return Node
-	 */
-	
-	public Node<K, V> searchNode(K k) {
-		return root==null ? null : root.searchNode(k);
-	}
-	
-	/**
-	 * Deletes a node from the binary search tree
-	 * @param k Key
-	 */
-	
-	public void deleteNode(K k) {
-		root.deleteNode(k);
-	}
-	
-	/**
-	 * Orders the binary search tree inorder
-	 * @param collection
-	 */
-	
-	public void inOrder(Collection<V> collection) {
-		root.inOrder(collection);
-	}
-	
-	/**
-	 * Orders the binary search tree preorder
-	 * @param collection
-	 */
-	
-	public void preOrder(Collection<V> collection) {
-		root.preOrder(collection);
-	}
-	
-	/**
-	 * Orders the binary search tree postorder
-	 * @param collection
-	 */
-	
-	public void postOrder(Collection<V> collection) {
-		root.postOrder(collection);
-	}
-	
-	/**
-	 * Calculates the weight of the binary search tree
-	 * @return Binary search tree's weight
-	 */
-	
-	public int getWeight() {
-		
-		if(root==null)
-			return 0;
-		else
-			weight = root.getWeight();
-		
-		return weight;
-	}
-	
-	/**
-	 * Calculates the height of the binary search tree
-	 * @return Binary search tree's height
-	 */
-	
-	public int getHeight() {
-		
-		if(root==null)
-			return 0;
-		else
-			height = root.getHeight();
-		
-		return height;
-=======
-		
-	/**
-	 * Adds a new node to the binary search tree
-	 * <b>post:</b> A new node has been added<br>
-	 * @param k Key
-	 * @param v Value
-	 * @throws Exception
-	 */
-	
-	public void addNode(K k, V v) throws Exception {
-		Node<K, V> node = new Node<>(k, v);
-		if(root==null)
-			root = node;
-		else
-			addNode(root, node);
-	}
-	
-	private void addNode(Node<K, V> current, Node<K, V> node) throws Exception {
-		
-		if(current.getK().compareTo(node.getK())==0)
-			throw new Exception("Element already inserted in the tree");
-		
-		if(current.getK().compareTo(node.getK())>0) {
-			
-			if(current.getLeft()==null) 
-				current.setLeft(node);
-			else
-				addNode(current.getLeft(), node);
-		}else {
-			
-			if(current.getLeft()==null) 
-				current.setRight(node);
-			else
-				addNode(current.getRight(), node);
+		if(root == null) {
+			root = tA;
+			return true;
 		}
+		
+		Node<K, V> current = root;
+		boolean added = false;
+		
+		while(!added){
+			if(tA.getK().compareTo(current.getK())<0) {
+				if(current.getLeft() != null) {
+					current = current.getLeft();
+				}else {
+					current.setLeft(tA);
+					tA.setParent(current);
+					added = true;
+				}
+			}else {
+				if(current.getRight() != null){
+					current = current.getRight();
+				}else {
+					current.setRight(tA);
+					tA.setParent(current);
+					added = true;
+				}
+			}
+		}
+		return added;
+		
 	}
 	
 	/**
@@ -255,7 +177,21 @@ public class BinarySearchTree<K extends Comparable<K>, V> implements BinarySearc
 	}
 	
 	protected void rotateLeft(Node<K, V> node) {
-		
+		Node<K, V> aux= node.getRight();
+		node.setRight(aux.getLeft());
+		if(aux.getLeft()!=null) {
+			aux.getLeft().setParent(node);
+		}
+		aux.setParent(node.getParent());
+		if(node.getParent()== null) {
+			root= aux;
+		}else if(node== node.getParent().getLeft()) {
+			node.getParent().setLeft(aux);
+		}else {
+			node.getParent().setRight(aux);
+		}
+		aux.setLeft(node);
+		node.setParent(aux);
 	}
 	
 	public void rotateRight(K key) {
@@ -279,6 +215,5 @@ public class BinarySearchTree<K extends Comparable<K>, V> implements BinarySearc
 		}
 		aux.setRight(node);
 		node.setParent(aux);
->>>>>>> branch 'master' of https://github.com/FernandaRojas152/people-database.git
 	}
 }
