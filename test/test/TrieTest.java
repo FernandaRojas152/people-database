06138dc92d6b34247a5b668f0dadd9305a5e2dc7
FernandaRojas152 <FernandaRojas152@hotmail.com>
2020-10-28 00:19:31 -0500

    test were added for trie's class

--- test/test/TrieTest.java
+++ test/test/TrieTest.java
@@ -1,16 +1,13 @@
 package test;
 
 import static org.junit.jupiter.api.Assertions.*;
-
 import java.util.List;
-
 import org.junit.jupiter.api.Test;
-
 import trie.Trie;
 
 class TrieTest {
 	Trie trie;
-	
+
 	/** STAGES */
 	public void setUpStage1() {
 		trie = new Trie();
@@ -24,18 +21,30 @@ class TrieTest {
 		trie.insert("JRR Tolkien");
 		trie.insert("Elvira Sastre");
 		trie.insert("Alejandra Pizarnik");
-		
 	}
-	
+
 	/** TESTS */
+	@Test
+	void testAutoCompleteWithSize() {
+		setUpStage1();
+		List<String> data= trie.autocomplete("E");
+		assertEquals(5, data.size(), "Should be 5");
+	}
+
 	@Test
 	void testAutoComplete() {
 		setUpStage1();
 		List<String> data= trie.autocomplete("E");
 		for (int i = 0; i < data.size(); i++) {
+			assertEquals('E', data.get(i).charAt(0));
 			System.out.println(data.get(i));
 		}
-		assertEquals(5, data.size(), "Should be 5");
 	}
-
+	
+	@Test
+	void testNoAutoComplete() {
+		setUpStage1();
+		List<String> data= trie.autocomplete("Y");
+		assertEquals(0, data.size(), "Should be 0");
+	}
 }
