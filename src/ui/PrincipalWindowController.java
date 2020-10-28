3244a75dfa4c1424663c0c6c738a92c05b505022
FernandaRojas152 <FernandaRojas152@hotmail.com>
2020-10-28 16:48:50 -0500

    some windows are added now, and default value to generate data it's now implemented

--- src/ui/PrincipalWindowController.java
+++ src/ui/PrincipalWindowController.java
@@ -1,20 +1,79 @@
 package ui;
 
+import java.util.List;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
+import trie.Trie;
 
 public class PrincipalWindowController {
-
-    @FXML
+	private Trie trie;
+	@FXML
     private TextField amountData;
 
+    @FXML
+    private TextField name;
+
+    @FXML
+    private TextField lastName;
+
+    @FXML
+    private TextField gender;
+
+    @FXML
+    private TextField birthdate;
+
+    @FXML
+    private TextField height;
+
+    @FXML
+    private TextField nationality;
+
+    @FXML
+    private ImageView photo;
+
+    @FXML
+    private GridPane container;
+
+    @FXML
+    private TextField auto;
+
+    @FXML
+    private VBox suggestions;
+    
     public PrincipalWindowController() {
-		// TODO Auto-generated constructor stub
 	}
     
     @FXML
     public void initialize() {
-		// TODO Auto-generated method stub
-
+		trie= new Trie();
+		trie.insert("Edgar Allan Poe");
+		trie.insert("James Barrie");
+		trie.insert("Emily Bronte");
+		trie.insert("Euripides");
+		trie.insert("Ernest Hemingway");
+		trie.insert("Arthur Conan Doyle");
+		trie.insert("Lewis Carroll");
+		trie.insert("JRR Tolkien");
+		trie.insert("Elvira Sastre");
+		trie.insert("Alejandra Pizarnik");
+		auto= new TextField();
+		String name= auto.getText();
+		List<String> data= trie.autocomplete(name);
+		suggestions= new VBox();
+		for (int i = 0; i < data.size(); i++) {
+			Label l= new Label(data.get(i));
+			suggestions.getChildren().add(l);
+		}
 	}
+    
+    @FXML
+    void generateData(ActionEvent event) {
+
+    }
 }
