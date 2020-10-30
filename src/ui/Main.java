ad5ab5e7d4e3f88c4ee8c6ac0dfddd2dd8a2d90a
jdpv01 <juandpv01@hotmail.com>
2020-10-29 21:33:29 -0500

    persistance method implemented

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -30,13 +30,7 @@ public class Main extends Application{
 
 				@Override
 				public void handle(WindowEvent arg0) {
-					// TODO Auto-generated method stub
-					try {
-						principal.saveData();
-					} catch (IOException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
+					principal.saveData();
 				}
 			});
 		} catch(Exception e) {
