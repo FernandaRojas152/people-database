181609627714e316fc1d1e1023b171f402b9f4d2
FernandaRojas152 <FernandaRojas152@hotmail.com>
2020-11-06 23:44:28 -0500

    some minor changes were maded

--- test/test/DatabaseTest.java
+++ test/test/DatabaseTest.java
@@ -1,9 +1,8 @@
 package test;
 
 import static org.junit.jupiter.api.Assertions.*;
-
+import java.time.LocalDate;
 import org.junit.jupiter.api.Test;
-
 import model.Database;
 
 class DatabaseTest {
@@ -15,10 +14,39 @@ class DatabaseTest {
 		database.createPerson("Fernanda", "Rojas", "Colombian");
 	}
 	
-	@Test
-	void test() {
-		setUpStage1();
-		assertEquals("Fernanda Rojas",database.searchByFullName("Fernanda Rojas"), "should be found");
+	public void setUpStage2() {
+		database= new Database();
+		database.createPerson("Amanda", "Rojas", "Female", LocalDate.now(), 1.62, "Colombian");
 	}
-
+	
+	public void setUpStage3() {
+		database= new Database();
+	}
+	
+	@Test
+	void testsearchByFullName() {
+		setUpStage1();
+		assertEquals("Fernanda",database.searchByFullName("Fernanda Rojas").getName(), "should be found");
+	}
+	
+	@Test
+	void testsearchByName() {
+		setUpStage1();
+		assertEquals("Fernanda",database.searchByName("Fernanda Rojas").getName(), "should be found");
+	}
+	
+	@Test
+	void testsearchByCode() {
+		setUpStage2();
+		assertEquals("Rojas",database.searchByLastName("Rojas").getLastName(), "should be found");
+	}
+	
+	@Test
+	void testDelete() {
+		setUpStage2();
+		database.createPerson("Carlos", "Perez", "Male", LocalDate.now(), 1.82, "Canadian");
+		database.searchByName("Carlos").setCode("abcde");
+		database.deletePerson("Carlos", "Perez", "abcde");
+		assertEquals(null,database.searchByName("Carlos"), "shouldn't be found");
+	}
 }
