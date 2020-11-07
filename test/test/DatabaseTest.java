d58892b9c1a571efd34e84b7e99194b73b125582
FernandaRojas152 <FernandaRojas152@hotmail.com>
2020-11-06 19:13:57 -0500

    trie diagram added

--- test/test/DatabaseTest.java
+++ test/test/DatabaseTest.java
@@ -1,6 +1,24 @@
 package test;
 
-public class DatabaseTest {
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import model.Database;
+
+class DatabaseTest {
+	private Database database;
 	
+	/** STAGES */
+	public void setUpStage1() {
+		database= new Database();
+		database.createPerson("Fernanda", "Rojas", "Colombian");
+	}
+	
+	@Test
+	void test() {
+		setUpStage1();
+		assertEquals("Fernanda Rojas",database.searchByFullName("Fernanda Rojas"), "should be found");
+	}
 
 }
